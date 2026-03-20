7d81d38d8d5050f5c504decf0f60484b3b810d1f
sonia250 <s.uwase3@alustudent.com>
2026-03-20 19:33:50 +0200

    add notebook with outputs

---
+++ summative/FlutterApp/stem_predictor/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\H O P E\\OneDrive\\Desktop\\linear_regression_model\\summative\\FlutterApp\\stem_predictor" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\H O P E\\OneDrive\\Desktop\\linear_regression_model\\summative\\FlutterApp\\stem_predictor"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\H O P E\\OneDrive\\Desktop\\linear_regression_model\\summative\\FlutterApp\\stem_predictor\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\H O P E\\OneDrive\\Desktop\\linear_regression_model\\summative\\FlutterApp\\stem_predictor"
+  "FLUTTER_TARGET=C:\\Users\\H O P E\\OneDrive\\Desktop\\linear_regression_model\\summative\\FlutterApp\\stem_predictor\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OGI4NzI4Njg0OQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzhmYzMwMTJlNA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC43"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\H O P E\\OneDrive\\Desktop\\linear_regression_model\\summative\\FlutterApp\\stem_predictor\\.dart_tool\\package_config.json"
+)
